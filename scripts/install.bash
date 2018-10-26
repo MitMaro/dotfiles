d2ddc9b0c179c1516cdc26343afd76a193000a0b
Tim Oram <dev@mitmaro.ca>
2018-10-26 16:31:51 -0230

    Add git-prompt.sh to .local/share/

--- scripts/install.bash
+++ scripts/install.bash
@@ -25,11 +25,14 @@ rm -rf "$__DOTS_DIR/config/"
 # create directories that will contain symlinks
 mkdir -p "$__DOTS_DIR/config/.config/terminator/plugins"
 mkdir -p "$__DOTS_DIR/config/.local/share/gnome-shell/extensions/"
+mkdir -p "$__DOTS_DIR/config/.local/share/git/"
 
 ln -sfn "$__DOTS_DIR/dependencies/diff-so-fancy/diff-so-fancy" "$__DOTS_DIR/bin/.bin/diff-so-fancy"
 ln -sfn "$__DOTS_DIR/dependencies/gnome-shell-audio-output-switcher" "$__DOTS_DIR/config/.local/share/gnome-shell/extensions/gnome-shell-audio-output-switcher@kgaut"
 ln -sfn "$__DOTS_DIR/dependencies/multi-monitors-add-on/multi-monitors-add-on@spin83" "$__DOTS_DIR/config/.local/share/gnome-shell/extensions/multi-monitors-add-on@spin83"
 ln -sfn "$__DOTS_DIR/dependencies/ide-sync/ide-sync.sh" "$__DOTS_DIR/bin/.bin/ide-sync"
+
+curl -o "$__DOTS_DIR/config/.local/share/git/git-prompt.sh" "https://raw.githubusercontent.com/git/git/master/contrib/completion/git-prompt.sh"
 
 stow ${stow_args} config
 stow ${stow_args} bin
