7a569cec487c1fc5b07e52a287e9f7c9739940c1
Tim Oram <dev@mitmaro.ca>
2019-01-09 14:41:42 -0330

    Improve messaging during install

--- scripts/install.bash
+++ scripts/install.bash
@@ -56,7 +56,9 @@ echo "Loading gsettings"
 if [[ "$SHELL" != "/bin/zsh" ]]; then
 	echo "Updating shell to zsh"
 	chsh -s "/bin/zsh"
-	echo "You will need to logout and login again for the change to take effect"
+	echo "You will need to logout and login again for the SHELL change to take effect"
 fi
+
+echo "If new gnome extensions were installed, you will need to restart gnome with the \`r\` command."
 
 echo "Install complete"
