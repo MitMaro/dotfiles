240443cc125da46454f2395681aea6d43e35e398
Tim Oram <dev@mitmaro.ca>
2019-05-23 21:28:49 -0230

    Remove lesspipe

--- dotprofile/lib/linux/010-lesspipe.bash
+++
@@ -1,6 +0,0 @@
-
-if [[ ! -x "/usr/bin/lesspipe" ]]; then
-	__DEBUG_MESSAGE "lesspipe not installed, skipping."
-	return
-fi
-eval "$(SHELL=/bin/sh lesspipe)"
--- dotprofile/lib/osx/010-lesspipe.bash
+++
@@ -1,6 +0,0 @@
-
-if [ ! -x /usr/local/bin/lesspipe.sh ]; then
-	__DEBUG_MESSAGE "lesspipe not installed, skipping."
-	return
-fi
-eval "$(SHELL=/bin/sh lesspipe.sh)"
